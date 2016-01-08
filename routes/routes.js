0e7722d117b18ba7efa0a7a17bddb6e13ea62302
Chris Quintanilla <Ca.q@live.com>
2016-01-08 13:36:21 -0700

    Established DB Connection Beginning User account crud

--- routes/routes.js
+++ routes/routes.js
@@ -1,4 +1,32 @@
+var mongoose = require('mongoose'),
+    uriUtil = require('mongodb-uri');
+
+var mongodbUri = 'mongodb://ds051740.mongolab.com:51740/capstonedb';
+var mongooseUri = uriUtil.formatMongoose(mongodbUri);
+var options = {
+  user: 'backenduser',
+  pass: 'password'
+}
+mongoose.connect(mongooseUri, options);
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error'));
+db.once('open', function (callback) {
+    console.log('DB Connection is Open');
+});
+
+//Schemas
+var podcastSchema = mongoose.Schema({});
+var podcastSubscriptionScema = mongoose.Schema({});
+var userSchema = mongoose.Schema({
+    name: String,
+    email: String,
+    password: String
+    
+});
+
 var view_directory = __dirname.replace('routes', 'views');
+
+
 exports.index = function (req,res) {
     var options = {
         root: view_directory,
@@ -21,3 +49,4 @@ exports.userRegister = function (req,res) {
     }
     res.sendFile('/user-register.html', options);
 }
+exports.userRegisterPost
