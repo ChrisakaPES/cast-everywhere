35d568119bb15ec0a789e29b7b9f7bfb0e6c761b
Chris Quintanilla <Ca.q@live.com>
2016-01-14 12:04:20 -0700

    Audio Player Plays mp3 audio

---
+++ public/build/scripts/jplayerinstance.js
@@ -0,0 +1,39 @@
+//$(document).ready(function() {
+//    $("#jquery_jplayer_1").jPlayer({
+//        ready: function() {
+//            $(this).jPlayer("setMedia", {
+//                title: "MeowTest",
+//                m4a: "http://www.jplayer.org/audio/m4a/Miaow-07-Bubble.m4a",
+//                oga: "http://www.jplayer.org/audio/ogg/Miaow-07-Bubble.ogg"
+//            });
+//        },
+//        cssSelectorAncestor: "#jp_container_1",
+//        swfPath: "/swf",
+//        supplied: "m4a, ogg",
+//        useStateClassSkin: true,
+//        autoblur: false,
+//        smoothPlayBar: true,
+//        keyEnabled: true,
+//        remainingDuration: true,
+//        toggleDuration: true
+//    });
+//});
+$(document).ready(function () {
+    $("#jquery_jplayer_1").jPlayer({
+        ready: function () {
+            $(this).jPlayer("setMedia", {
+                title: "Giant Bombcast",
+                mp3: "http://www.giantbomb.com/podcasts/download/1473/Giant_Bombcast_01_12_2016-01-12-2016-0020062817.mp3"
+            });
+        },
+        cssSelectorAncestor: "#jp_container_1",
+        swfPath: "/swf",
+        supplied: "mp3",
+        useStateClassSkin: true,
+        autoblur: false,
+        smoothPlayBar: true,
+        keyEnabled: true,
+        remainingDuration: true,
+        toggleDuration: true
+    });
+});
